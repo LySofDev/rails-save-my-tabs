2cdadde881a6af45baea1ae0d855b02f4eef4b0a
Esteban Hernandez <hernandez.dev.services@gmail.com>
2018-07-16 11:07:07 -0400

    Disabled seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-def test_user
-  User.create({
-    email: "mac.hdz@gmail.com",
-    password: "password",
-    password_confirmation: "password"
-  })
-end
-
-user = test_user
-
-50.times { FactoryBot.create(:tab, user: user) }
+# def test_user
+#   User.create({
+#     email: "mac.hdz@gmail.com",
+#     password: "password",
+#     password_confirmation: "password"
+#   })
+# end
+#
+# user = test_user
+#
+# 50.times { FactoryBot.create(:tab, user: user) }
