31132fb411e595e8be7f76a52b4bb468f0312ff6
Esteban Hernandez <hernandez.dev.services@gmail.com>
2018-08-07 16:58:53 -0400

    Removed origins restrictions

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://save-my-tabs.surge.sh'
+    origins '*'
 
     resource '*',
       headers: :any,
