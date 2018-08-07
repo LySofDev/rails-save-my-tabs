9a38f84e5b4859d5ca6f124923fda1d66f9dc8f4
Esteban Hernandez <hernandez.dev.services@gmail.com>
2018-08-07 16:02:34 -0400

    Added tests for tabs#destroy

--- spec/controllers/tabs_controller_spec.rb
+++ spec/controllers/tabs_controller_spec.rb
@@ -182,4 +182,191 @@ RSpec.describe TabsController, type: :controller do
 
   end
 
+  describe "#update" do
+
+    before :each do
+      @user = create(:user)
+      @tab = create(:tab, user: @user)
+      @tab_data = { url: "https://www.google.com" }
+    end
+
+    context "without a security token" do
+
+      it "returns a 401 code" do
+        patch :update, params: { id: @tab.id, tab: @tab_data }
+        expect(response.status).to be 401
+      end
+
+      it "doesn't update the tab" do
+        patch :update, params: { id: @tab.id, tab: @tab_data }
+        tab = Tab.find(@tab.id)
+        expect(tab.url).not_to eq @tab_data[:url]
+      end
+
+    end
+
+    context "with a different user identity" do
+
+      before :each do
+        user = create(:user)
+        request.headers.merge({ "Authorization" => "Beare #{user.as_token}" })
+      end
+
+      it "returns a 403 code" do
+        patch :update, params: { id: @tab.id, tab: @tab_data }
+        expect(response.status).to be 403
+      end
+
+      it "doesn't update the tab" do
+        patch :update, params: { id: @tab.id, tab: @tab_data }
+        tab = Tab.find(@tab.id)
+        expect(tab.url).not_to eq @tab_data[:url]
+      end
+
+    end
+
+    context "with valid update data" do
+
+      before :each do
+        request.headers.merge({ "Authorization" => "Beare #{@user.as_token}" })
+      end
+
+      it "returns a 200 code" do
+        patch :update, params: { id: @tab.id, tab: @tab_data }
+        expect(response.status).to eq 200
+      end
+
+      it "updates the tab" do
+        patch :update, params: { id: @tab.id, tab: @tab_data }
+        tab = Tab.find(@tab.id)
+        expect(tab.url).to eq @tab_data[:url]
+      end
+
+    end
+
+    context "with invalid update data" do
+
+      before :each do
+        @tab_data[:url] = ""
+        request.headers.merge({ "Authorization" => "Beare #{@user.as_token}" })
+      end
+
+      it "returns a 422 code" do
+        patch :update, params: { id: @tab.id, tab: @tab_data }
+        expect(response.status).to eq 422
+      end
+
+      it "doesn't update the tab" do
+        patch :update, params: { id: @tab.id, tab: @tab_data }
+        tab = Tab.find(@tab.id)
+        expect(tab.url).not_to eq ""
+      end
+
+    end
+
+  end
+
+  describe "#show" do
+
+    before :each do
+      @user = create(:user)
+      @tab = create(:tab, user: @user)
+    end
+
+    context "without a security token" do
+      it "returns a 401 code" do
+        get :show, params: { id: @tab.id }
+        expect(response.status).to eq 401
+      end
+    end
+
+    context "with a different user identity" do
+
+      before :each do
+        user = create(:user)
+        request.headers.merge({ "Authorization" => "Beare #{user.as_token}" })
+      end
+
+      it "returns a 403 code" do
+        get :show, params: { id: @tab.id }
+        expect(response.status).to eq 403
+      end
+
+    end
+
+    context "with a matching user identity" do
+
+      before :each do
+        request.headers.merge({ "Authorization" => "Beare #{@user.as_token}" })
+      end
+
+      it "returns a 200 code" do
+        get :show, params: { id: @tab.id }
+        expect(response.status).to eq 200
+      end
+
+      it "returns the tab url" do
+        get :show, params: { id: @tab.id }
+        json = JSON.parse(response.body)
+        expect(json["url"]).to eq @tab.url
+      end
+
+      it "returns the tab title" do
+        get :show, params: { id: @tab.id }
+        json = JSON.parse(response.body)
+        expect(json["title"]).to eq @tab.title
+      end
+
+    end
+
+  end
+
+  describe "#destroy" do
+
+    before :each do
+      @user = create(:user)
+      @tab = create(:tab, user: @user)
+    end
+
+    context "without a security token" do
+      it "returns a 401 code" do
+        delete :destroy, params: { id: @tab.id }
+        expect(response.status).to eq 401
+      end
+    end
+
+    context "with an incorrect user identity" do
+
+      before :each do
+        user = create(:user)
+        request.headers.merge({ "Authorization" => "Beare #{user.as_token}" })
+      end
+
+      it "returns a 403 code" do
+        delete :destroy, params: { id: @tab.id }
+        expect(response.status).to eq 403
+      end
+    end
+
+    context "with a correct user identity" do
+
+      before :each do
+        request.headers.merge({ "Authorization" => "Beare #{@user.as_token}" })
+      end
+
+      it "returns a 200 code" do
+        delete :destroy, params: { id: @tab.id }
+        expect(response.status).to eq 200
+      end
+
+      it "destroys the tab record" do
+        delete :destroy, params: { id: @tab.id }
+        matching_tabs = Tab.where(id: @tab.id).count
+        expect(matching_tabs).to eq 0
+      end
+
+    end
+    
+  end
+
 end
